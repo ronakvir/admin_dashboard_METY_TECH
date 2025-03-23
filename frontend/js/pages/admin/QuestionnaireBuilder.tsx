7a5e0a0ba3975ff73d8ae0af748e7d72c681dcdb
Joshua Burgess <josh.burgess0011@gmail.com>
2025-03-22 18:01:43 -0700

    Many errors, just saving my work

--- frontend/js/pages/admin/QuestionnaireBuilder.tsx
+++ frontend/js/pages/admin/QuestionnaireBuilder.tsx
@@ -1,13 +1,17 @@
 import { Console } from "console";
-import React, { useState } from "react";
+import React, { ButtonHTMLAttributes, ChangeEvent, useState } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 
 
 const QuestionnaireBuilder: React.FC = () => {
-  // Holds the CURRENT question data
-  const [formData, setFormData] = useState({ question: "", answers: [""] });
   // Contains the state of what type of question is being added
   const [selectedOption, setSelectedOption] = useState("text");
+  
+  // Holds the CURRENT question data
+  const [formData, setFormData] = useState({ type: selectedOption, question: "", answers: [""] });
+
+  const [questionnaireList, setQuestionnaireList] = useState({});
+  
   
   // These are placeholder until I figure out how to do database things
   const [questions, setQuestions] = useState([formData]);
@@ -20,7 +24,8 @@ const QuestionnaireBuilder: React.FC = () => {
   const showQuestions = () => {
     return questions.map((question, index) => (
       <div>
-        <p >Question {index+1}: {question.question}</p>
+        <p>Type {index+1}: {question.type}</p>
+        <p>Question {index+1}: {question.question}</p>
         {question.answers.map((answer, answerIndex) => (
           <p key={answerIndex}>Answer {answerIndex+1}: {answer}</p>
         ))}
@@ -30,7 +35,7 @@ const QuestionnaireBuilder: React.FC = () => {
 
   // Updates the formData data everytime they type in a form field
   const changeFormData = (value: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData({ ...formData, [value.target.name]: value.target.value });
+    setFormData({ ...formData, type: formData.type,[value.target.name]: value.target.value });
   }
 
   // Updates the formData answers every time a answer field is added or removed
@@ -38,7 +43,7 @@ const QuestionnaireBuilder: React.FC = () => {
     const newAnswers = [...formData.answers];
     newAnswers[index] = value;
     setFormData({ ...formData, answers: newAnswers });
-  };
+  }
   
   // changes the page state when a dropdown menu item is selected
   const changeDropdown = async (value: React.ChangeEvent<HTMLSelectElement>) => {
@@ -60,13 +65,23 @@ const QuestionnaireBuilder: React.FC = () => {
     setFormData({ ...formData, answers: updatedAnswers })
   }
 
+
+  // Adds the completed question to the questions array.
+  // This will eventually be changed to adding it to the database.
+  const tempAddQuestion = () => {
+    if (formData.question.trim() == "") return;
+
+    setQuestions([ ...questions, formData ]);
+    setFormData({ type: "", question: "", answers: [""] });
+  }; 
+
   // Adds the completed question to the questions array.
   // This will eventually be changed to adding it to the database.
   const addQuestion = () => {
     if (formData.question.trim() == "") return;
 
     setQuestions([ ...questions, formData ]);
-    setFormData({ question: "", answers: [""] });
+    setFormData({ type: "", question: "", answers: [""] });
   }; 
 
   // Switch-Case to determine which add-question fields should be showm
@@ -100,15 +115,86 @@ const QuestionnaireBuilder: React.FC = () => {
   }
 
 
+  const recentQuestionnaires = () =>{
+    const questionnaireCards = [
+      {name: "Questionnaire Name", questions: "12", status: "Active", responses: "1005"},
+      {name: "Questionnaire Name", questions: "15", status: "Template", responses: "52"},
+      {name: "Questionnaire Name", questions: "13", status: "Inactive", responses: "135"},
+      {name: "Questionnaire Name", questions: "10", status: "Active", responses: "521"}
+    ];
+    return (
+      <div style={{display: "flex", flexDirection: "column"}}>
+        <h4 style={{display: "flex", justifyContent: "left"}}>Recent Questionnaires</h4>
+        <div style={{display: "flex", flexDirection: "row"}}>
+          {questionnaireCards.map((questionnaire) => (
+            <div style={{width: "200px", height: "250px", backgroundColor: "lightgrey", borderRadius: "15px", overflow: "hidden", margin: "5px", padding: "10px"}}>
+              <h3>{questionnaire.name}</h3>
+              <p>{questionnaire.questions} questions - {questionnaire.status} - {questionnaire.responses} responses</p>
+              <button>
+                Edit
+              </button>
+              <button>
+                Preview
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    )
+  }
+
+  let questionnaireListVariable: JSX.Element = <></>;
+  const questionnaireList = (scope: string) => {
+    const questionnaireCards = [
+      {name: "Questionnaire Name", questions: "12", status: "Active", responses: "1005"},
+      {name: "Questionnaire Name", questions: "15", status: "Template", responses: "52"},
+      {name: "Questionnaire Name", questions: "13", status: "Inactive", responses: "135"},
+      {name: "Questionnaire Name", questions: "10", status: "Active", responses: "521"}
+    ];
+
+    return
+      <div style={{display: "flex", flexDirection: "column"}}>
+        <h4 style={{display: "flex", justifyContent: "left"}}>{scope} Questionnaires</h4>
+        <div style={{display: "flex", flexDirection: "row"}}>
+          {questionnaireCards.map((questionnaire) => (
+            <div style={{width: "200px", height: "250px", backgroundColor: "lightgrey", borderRadius: "15px", overflow: "hidden", margin: "5px", padding: "10px"}}>
+              <h3>{questionnaire.name}</h3>
+              <p>{questionnaire.questions} questions - {questionnaire.status} - {questionnaire.responses} responses</p>
+              <button>
+                Edit
+              </button>
+              <button>
+                Preview
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    
+  }
+
+
+
+
+
   return (
     <>
       <h1>Questionnaire Builder</h1>
+      {recentQuestionnaires()}
+      <div style={{display: "flex"}}>
+        <button onClick={() => questionnaireList("All")}>All Questionnaires</button>
+        <button onClick={() => questionnaireList("Template")}>Templates</button>
+        <button onClick={() => questionnaireList("Published")}>Published</button>
+        <button onClick={() => questionnaireList("Drafts")}>Drafts</button>
+      </div>
+      {questionnaireListVariable}
       <select style={{display: "inline"}} value={selectedOption} onChange={changeDropdown}>
         <option value="text">Text Input</option>
         <option value="multichoice">Multiple Choice</option>
         <option value="slider">Slider Value</option>
         <option value="checkbox">Checkbox</option>
       </select>
+      
       {questionForms}
       <hr/>
       <h3>Questions List</h3>
