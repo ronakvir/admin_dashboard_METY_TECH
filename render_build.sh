b49b58f9f171e7a29240f1f897f268af842c72d0
Ronak Vir <ronakpvir@gmail.com>
2025-09-07 20:12:39 -0500

    ok this gotta work

--- render_build.sh
+++ render_build.sh
@@ -34,9 +34,11 @@ if [ -n "${AUTO_MIGRATE:-}" ] && [ "${AUTO_MIGRATE:-}" == 1 ] && [ -n "$DATABASE
 fi
 
 echo "-----> Pushing source maps to Sentry"
-if [ -n "$SENTRY_API_KEY" ] && [ -n "$SENTRY_ORG" ] && [ -n "$SENTRY_PROJECT_NAME" ] && [ -n "$RENDER_GIT_COMMIT" ]; then
+if [ -n "${SENTRY_API_KEY:-}" ] && [ -n "${SENTRY_ORG:-}" ] && [ -n "${SENTRY_PROJECT_NAME:-}" ] && [ -n "${RENDER_GIT_COMMIT:-}" ]; then
     npx @sentry/cli --auth-token=$SENTRY_API_KEY releases --org=$SENTRY_ORG --project=$SENTRY_PROJECT_NAME files $RENDER_GIT_COMMIT upload-sourcemaps ./frontend/webpack_bundles/ --url-prefix "~/static/webpack_bundles/" --rewrite
     rm ./frontend/webpack_bundles/*.js.map
+else
+    echo "-----> Skipping Sentry source maps (no SENTRY_API_KEY)"
 fi
 
 echo "-----> Post-compile done"
